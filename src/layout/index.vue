71b04f2e33206322d89b48358e201116ba4f461d
xbb <616749285@qq.com>
2025-06-26 13:54:52 +0800

    framework: 增加 vueuse

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,12 +1,14 @@
 <template>
-  <div>
-    <div>Header</div>
-    <div>
-      <div>Menu</div>
-      <div>
-        <router-view />
-      </div>
-    </div>
+  <div class="common-layout">
+    <el-container>
+      <el-header>Header</el-header>
+      <el-container>
+        <el-aside width="200px">Aside</el-aside>
+        <el-main>
+          <router-view />
+        </el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
